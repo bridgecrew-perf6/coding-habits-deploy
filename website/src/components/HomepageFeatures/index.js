678107ca13272e9cffd9500ff2bebbf4459c1ce3
Faiz Mohammed <faiz.mohammed@outlook.com>
2022-04-12 11:54:34 +0530

    Created CodingHabits website + Initial Version of HTML Tutotial

---
+++ website/src/components/HomepageFeatures/index.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.css';
+
+const FeatureList = [
+  {
+    title: 'JavaScript',
+    Svg: require('@site/static/img/javascript-ar21.svg').default,
+    imgUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/512px-Unofficial_JavaScript_logo_2.svg.png",
+    description: (
+      <>
+        Docusaurus was designed from the ground up to be easily installed and
+        used to get your website up and running quickly.
+      </>
+    ),
+  },
+  {
+    title: 'HTML',
+    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    imgUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/512px-Unofficial_JavaScript_logo_2.svg.png",
+    description: (
+      <>
+        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
+        ahead and move your docs into the <code>docs</code> directory.
+      </>
+    ),
+  },
+  {
+    title: 'DataStructures Using JavaScript',
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    imgUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/512px-Unofficial_JavaScript_logo_2.svg.png",
+    description: (
+      <>
+        Extend or customize your website layout by reusing React. Docusaurus can
+        be extended while reusing the same header and footer.
+      </>
+    ),
+  },
+];
+
+/*function Feature({Svg, title, description, imgUrl}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+        <img src={imgUrl}></img>
+      </div>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}*/
+
+function Feature({Svg, title, description, imgUrl}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <img src={imgUrl}></img>
+      </div>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
