a8f401610d9c2b36531bae713644b77eac91bcb0
jayaprakashajaishee <jayaprakashajaishee@gmail.com>
2022-04-28 12:56:14 +0530

    domain name changed in config

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Learn to Code',
   tagline: 'with coding habits',
-  url: 'https://codingHabits.netlify.app',
+  url: 'https://calm-cassata-c6bbae.netlify.app/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
